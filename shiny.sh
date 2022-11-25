a035a9cfdd97f0680452860feaaa40d260fb19fb
IIHermanII <108313459+IIHermanII@users.noreply.github.com>
2022-11-25 13:52:05 -0500

    Update shiny.sh

--- shiny.sh
+++ shiny.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Simple script for 1/50 chance of a shiny pokemon.
+# Uses pokemon-colorscripts
+
 num=$(( $RANDOM % 50 + 1 )) 
 if [ "$num" == "1" ]; then
   pokemon-colorscripts -n shaymin -s --no-title
